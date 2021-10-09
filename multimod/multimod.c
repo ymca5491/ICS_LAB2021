ca4255997b56ead25f1b2e0fa09359458165c1c5
tracer-nju <tracer@nju.edu.cn>
2021-10-09 15:41:34 +0800

    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: a8c6444724ad4045b9a65dda0c189ef0   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-36-generic       Architecture: x86-64  15:41:34 up  1:39,  2 users,  load average: 1.20, 1.18, 1.07

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -13,17 +13,17 @@ uint64_t mod(uint64_t a, uint64_t m) {
 
 uint64_t plusmod(uint64_t a, uint64_t b, uint64_t m) {
   /* a, b < m */
-  uint64_t t_ans = a + b;
-    if (t_ans < a || t_ans >= m) {
-      t_ans -= m;
+  uint64_t ans = a + b;
+    if (ans < a || ans >= m) {
+      ans -= m;
     }
-    return t_ans;
+    return ans;
 }
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
   uint64_t ans = 0;
   a = mod(a, m);
-  for (; b; b >> 1) {
+  for (; b; b >>= 1) {
     if (b & 1) {
       ans = plusmod(ans, a, m);
     }
