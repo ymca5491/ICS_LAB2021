194c8231ecc7b889cdcb7a2a818a8e4d9cbfdaec
tracer-nju <tracer@nju.edu.cn>
2021-11-14 01:14:20 +0800

    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 82e8ac52b7684c7abe1b62b33289eede   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  01:14:20 up  1:09,  4 users,  load average: 0.48, 0.34, 0.29

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -22,7 +22,7 @@ int asm_popcnt(uint64_t x) {
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
   asm (
-    ".byte 0xf3a4" //"rep; movsb"
+    ".byte 0xf3 0xa4" //"rep; movsb"
     :
     :"c"(n)
     :"%rsi", "%rdi"
