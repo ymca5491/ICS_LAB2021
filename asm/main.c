4c48a98362b4deb0acf4d21b5589f9edfacc15b1
tracer-nju <tracer@nju.edu.cn>
2021-11-13 23:38:43 +0800

    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: e859615a552c4987820e33442b4b05ff   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  23:38:43 up 10 min,  3 users,  load average: 0.48, 0.54, 0.38

--- asm/main.c
+++ asm/main.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 
 int main() {
-  //asm_jmp_buf buf;
+  asm_jmp_buf buf;
   int a = 0;
   int b = 0xffff;
-  //int r = asm_setjmp(buf);
-  int r = 0;
+  int r = asm_setjmp(buf);
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
     assert(*(int*)asm_memcpy(&a, &b, 2) == 0xffff);
-    //asm_longjmp(buf, 123);
+    asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
     printf("PASSED.\n");
