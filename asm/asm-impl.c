52d9dcdf82d382f64a7afd484398608fd0d42a67
tracer-nju <tracer@nju.edu.cn>
2021-11-13 22:54:35 +0800

    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 902d53c629564a25a3b6c5249bba36e9   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  22:54:35 up 15 min,  3 users,  load average: 0.60, 0.64, 0.44

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -2,38 +2,44 @@
 #include <string.h>
 
 int64_t asm_add(int64_t a, int64_t b) {
+  /*
   asm (
     "addq %[b], %0"
     :"=r"(a)
     :"0"(a), [b] "r"(b)
   );
+  */
   return a;
 }
 
 int asm_popcnt(uint64_t x) {
   uint64_t s = 0;
+  /*
   asm (
     "popcntq %1, %0"
     :"=r"(s)
     :"r"(x)
   );
+  */
   return s;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
+  /*
   asm (
     "rep; movsb"
     :
     :"c"(n)
     :"%rsi", "%rdi"
   );
+  */
   return dest;
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  return 0;
+  return setjmp(env);
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  ;
+  longjmp(env, val);
 }
