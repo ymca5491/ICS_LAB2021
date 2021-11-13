2467182dc8f6302a2b4588fd8398cb5398d56cde
tracer-nju <tracer@nju.edu.cn>
2021-11-13 17:10:09 +0800

    Static hostname: ymca-xhan66          Icon name: computer-laptop            Chassis: laptop         Machine ID: 877a8ce459e84f16920d7c513f6290d0            Boot ID: 515474e1c5554c258a5e3875a9b4f7db   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  17:10:10 up  2:39,  3 users,  load average: 0.21, 0.35, 0.29

--- asm/main.c
+++ asm/main.c
@@ -10,8 +10,7 @@ int main() {
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-    asm_memcpy(&a, &b, 2);
-    assert(a == 0xffff);
+    assert(*(int*)asm_memcpy(&a, &b, 2) == 0xffff);
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
